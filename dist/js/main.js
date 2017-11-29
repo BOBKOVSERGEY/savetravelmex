16b1326881ab4df6cb2a605f4c109271f0473e86
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-11-29 16:24:22 +0300

    Новое меню

---
+++ dist/js/main.js
@@ -0,0 +1,84 @@
+/**
+ * Global variables
+ */
+"use strict";
+
+var userAgent = navigator.userAgent.toLowerCase(),
+  initialDate = new Date(),
+
+  $document = $(document),
+  $window = $(window),
+  $html = $("html"),
+
+  isDesktop = $html.hasClass("desktop"),
+  isIE = userAgent.indexOf("msie") != -1 ? parseInt(userAgent.split("msie")[1]) : userAgent.indexOf("trident") != -1 ? 11 : userAgent.indexOf("edge") != -1 ? 12 : false,
+  isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+  isTouch = "ontouchstart" in window,
+
+  plugins = {
+    pointerEvents: isIE < 11 ? "js/pointer-events.min.js" : false,
+    rdNavbar: $(".rd-navbar"),
+    navbarToggle: $(".rd-navbar-toggle")
+  };
+
+/**
+ * Initialize All Scripts
+ */
+$document.ready(function () {
+
+
+  /**
+   * IE Polyfills
+   * @description  Adds some loosing functionality to IE browsers
+   */
+  if (isIE) {
+    if (isIE < 10) {
+      $html.addClass("lt-ie-10");
+    }
+
+    if (isIE < 11) {
+      if (plugins.pointerEvents) {
+        $.getScript(plugins.pointerEvents)
+          .done(function () {
+            $html.addClass("ie-10");
+            PointerEventsPolyfill.initialize({});
+          });
+      }
+    }
+
+    if (isIE === 11) {
+      $("html").addClass("ie-11");
+    }
+
+    if (isIE === 12) {
+      $("html").addClass("ie-edge");
+    }
+  }
+
+
+  /**
+   * UI To Top
+   * @description Enables ToTop Button
+   */
+  if (isDesktop) {
+    $().UItoTop({
+      easingType: 'easeOutQuart',
+      containerClass: 'ui-to-top fa fa-angle-up'
+    });
+  }
+
+  /**
+   * RD Navbar
+   * @description Enables RD Navbar plugin
+   */
+  if (plugins.rdNavbar.length) {
+    plugins.rdNavbar.RDNavbar({
+      stickUpClone: (plugins.rdNavbar.attr("data-stick-up-clone")) ? plugins.rdNavbar.attr("data-stick-up-clone") === 'true' : false
+    });
+    if (plugins.rdNavbar.attr("data-body-class")) {
+      document.body.className += ' ' + plugins.rdNavbar.attr("data-body-class");
+    }
+  }
+
+});
+
