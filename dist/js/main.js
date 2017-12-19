89c89dfb3371df62563b34b6e7a10c057ff877d6
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-12-19 16:41:39 +0300

    img gallery

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,7 +19,9 @@ var userAgent = navigator.userAgent.toLowerCase(),
     pointerEvents: isIE < 11 ? "js/pointer-events.min.js" : false,
     rdNavbar: $(".rd-navbar"),
     navbarToggle: $(".rd-navbar-toggle"),
-    customWaypoints: $('[data-custom-scroll-to]')
+    customWaypoints: $('[data-custom-scroll-to]'),
+    dateCountdown: $('.DateCountdown'),
+    photoSwipeGallery: $("[data-photo-swipe-item]")
   };
 
 /**
@@ -254,7 +256,7 @@ $document.ready(function () {
       }
     }
   });
-
+/* swiper*/
   var swiper = new Swiper('.swiper-container', {
     loop: true,
     effect: 'fade',
@@ -268,5 +270,163 @@ $document.ready(function () {
     },
   });
 
+  /**
+   * TimeCircles
+   * @description  Enable TimeCircles plugin
+   */
+  if (plugins.dateCountdown.length) {
+    var i;
+    for (i = 0; i < plugins.dateCountdown.length; i++) {
+      var dateCountdownItem = $(plugins.dateCountdown[i]),
+        time = {
+          "Days": {
+            "text": "Дней",
+            "color": "#43d0d9",
+            "show": true
+          },
+          "Hours": {
+            "text": "Часов",
+            "color": "#43d0d9",
+            "show": true
+          },
+          "Minutes": {
+            "text": "Минут",
+            "color": "#43d0d9",
+            "show": true
+          },
+          "Seconds": {
+            "text": "Секунд",
+            "color": "#43d0d9",
+            "show": true
+          }
+        };
+      dateCountdownItem.TimeCircles({
+        fg_width: 0.025,
+        circle_bg_color: "#f4f4f4",
+        bg_width: 0.5
+      });
+      $(window).on('load resize orientationchange', function () {
+        if (window.innerWidth < 479) {
+          dateCountdownItem.TimeCircles({
+            time: {
+              Days: {
+                color: "#43d0d9",
+                show: true
+              },
+              Hours: {
+                color: "#43d0d9",
+                show: true
+              },
+              Minutes: {
+                color: "#43d0d9",
+                show: true
+              },
+              Seconds: {show: false}
+            }
+          }).rebuild();
+        } else if (window.innerWidth < 991) {
+          dateCountdownItem.TimeCircles({
+            time: {
+              Days: {
+                color: "#43d0d9",
+                show: true
+              },
+              Hours: {
+                color: "#43d0d9",
+                show: true
+              },
+              Minutes: {
+                color: "#43d0d9",
+                show: true
+              },
+              Seconds: {show: false}
+            }
+          }).rebuild();
+        } else {
+          dateCountdownItem.TimeCircles({time: time}).rebuild();
+        }
+      });
+    }
+  }
+
+  /**
+   * PhotoSwipe Gallery
+   * @description Enables PhotoSwipe Gallery plugin
+   */
+  if (plugins.photoSwipeGallery.length) {
+
+    // init image click event
+    $document.delegate("[data-photo-swipe-item]", "click", function (event) {
+      event.preventDefault();
+
+      var $el = $(this),
+        $galleryItems = $el.parents("[data-photo-swipe-gallery]").find("a[data-photo-swipe-item]"),
+        pswpElement = document.querySelectorAll('.pswp')[0],
+        encounteredItems = {},
+        pswpItems = [],
+        options,
+        pswpIndex = 0,
+        pswp;
+
+      if ($galleryItems.length == 0) {
+        $galleryItems = $el;
+      }
+
+      // loop over the gallery to build up the photoswipe items
+      $galleryItems.each(function () {
+        var $item = $(this),
+          src = $item.attr('href'),
+          size = $item.attr('data-size').split('x'),
+          pswdItem;
+
+        if ($item.is(':visible')) {
+
+          // if we have this image the first time
+          if (!encounteredItems[src]) {
+            // build the photoswipe item
+            pswdItem = {
+              src: src,
+              w: parseInt(size[0], 10),
+              h: parseInt(size[1], 10),
+              el: $item // save link to element for getThumbBoundsFn
+            };
+
+            // store that we already had this item
+            encounteredItems[src] = {
+              item: pswdItem,
+              index: pswpIndex
+            };
+
+            // push the item to the photoswipe list
+            pswpItems.push(pswdItem);
+            pswpIndex++;
+          }
+        }
+      });
+
+      options = {
+        index: encounteredItems[$el.attr('href')].index,
+
+        getThumbBoundsFn: function (index) {
+          var $el = pswpItems[index].el,
+            offset = $el.offset();
+
+          return {
+            x: offset.left,
+            y: offset.top,
+            w: $el.width()
+          };
+        }
+      };
+
+      // open the photoswipe gallery
+      pswp = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, pswpItems, options);
+      pswp.init();
+    });
+  }
+
+
 });
+
+
 
